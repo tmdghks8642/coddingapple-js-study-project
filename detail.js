64b50d73e59b82cf55e290ee07e3f3d23f5e0acd
이승환 <tmdghks8642@gmail.com>
2023-04-01 10:45:46 +0900

    배열,객체의 데이터 바인딩

--- detail.js
+++ detail.js
@@ -7,28 +7,16 @@ const content2 = document.querySelectorAll('.tab-content')[1];
 const butn3 = document.querySelectorAll('.tab-button')[2];
 const content3 = document.querySelectorAll('.tab-content')[2];
  
-butn1.addEventListener('click',()=>{ // 1번째 버튼
-    //모든 orange 제거 후  
-    //해당 버튼에 orange 추가 
-    butnAll.forEach(el=>{el.classList.remove('orange')});
-    butn1.classList.add('orange');
-    console.log(butnAll.length)
-    // 모든 div 박스에 붙은 show 제거 
-    // 해당 버튼에 show 붙이기
-    contentAll.forEach(el=>{el.classList.remove('show')});
-    content1.classList.add('show');
-})
+const allButton = document.querySelector('.list')
 
-butn2.addEventListener('click',()=>{ // 2번쨰 버튼
+function butClick(i){
     butnAll.forEach(el=>{el.classList.remove('orange')});
-    butn2.classList.add('orange');
+    document.querySelectorAll('.tab-button')[i].classList.add('orange');
     contentAll.forEach(el=>{el.classList.remove('show')});
-    content2.classList.add('show');
-})
+    document.querySelectorAll('.tab-content')[i].classList.add('show');
 
-butn3.addEventListener('click',()=>{ // 3번째 버튼
-    butnAll.forEach(el=>{el.classList.remove('orange')});
-    butn3.classList.add('orange');
-    contentAll.forEach(el=>{el.classList.remove('show')});
-    content3.classList.add('show');
+}
+
+allButton.addEventListener('click',(e)=>{
+        butClick(e.target.dataset.id)
 })
